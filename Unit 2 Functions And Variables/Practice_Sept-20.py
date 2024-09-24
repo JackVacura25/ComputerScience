721b288e2040a65b8a615cd40d9c0a405682ce68
819581@mystma.org <819581@mystma.org>
2024-09-24 15:19:37 -0500

    Bla

---
+++ Unit 2 Functions And Variables/Practice_Sept-20.py
@@ -0,0 +1,4 @@
+favorite_animal = input("whats your favorite animal?\n>")
+reason = input("why is it your favorite animal?\n>")
+print(str"your favorite animal is {favorite_animal} because its {reason} thats cool")
+habitat = input("where does your favorite animal live?\n>")
---
+++ Unit 2 Functions And Variables/practice_sept-24.py
@@ -0,0 +1,5 @@
+my_variable = 4 + 5      #Decloration - Create
+print(my_variable)
+my_variable1 = 5 +18       #Assignment - modify
+print(my_variable1)
+print(my_variable + my_variable1)
