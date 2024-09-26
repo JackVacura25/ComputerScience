7aa2cf595872c634ec05720f46e150ce058c6b56
819581@mystma.org <819581@mystma.org>
2024-09-26 15:19:10 -0500

    madlib assignment

--- Assignments/animalsurvey.py
+++ Assignments/animalsurvey.py
@@ -1,6 +1,6 @@
-favorite_animal = input("whats your favorite animal?\n>")
-reason = input("why is it your favorite animal?\n>")
-print(f"your favorite animal is {favorite_animal} because its {reason} thats cool")
-habitat = input("where does your favorite animal live?\n>")
-Reason = input("Why does your favorite animal live there?\n>")
-print(f"so {favorite_animal}s live in {habitat} because {reason} thats very interesting")
+favorite_animal = input("whats your favorite animal?\n>") #asking user what thier fav animal is
+reason = input("why is it your favorite animal?\n>") # asking user why its thier fav
+print(f"your favorite animal is {favorite_animal} because its {reason} thats cool") # making a comment about thier animal
+habitat = input("where does your favorite animal live?\n>") # asking user where the animal lives
+Reason = input("Why does your favorite animal live there?\n>") # asking why it lives there
+print(f"so {favorite_animal}s live in {habitat} because {reason} thats very interesting") #making a conclusion on the animals habitat and commenting
---
+++ Unit 2 Functions And Variables/madlib.py
@@ -0,0 +1,23 @@
+#in the year [year], there there lived a man named [name] [noun]
+#[name] [noun] lives in [place] and works in [career]
+#[name] [noun] was sick of his [emotion] life so he set off on an adventure
+#after [number] years of searching for something exciting [name] [noun] met [ celeberity
+# [celeberity] challenged [name] [noun] to a [competition] 
+# turns out[name was a [word for good] copetition plural] so much so that after the [competition] [celeberity] wanted to get active
+# [celeberity] pulls out a [object] and swings it at [name]s [body part] 
+# [name] then delivers a devestating [strike of any sort] to [celeberity]s [body part] ending them
+year = input("enter a year\n>")
+name = input("enter a first name\n>")
+noun = input("enter a noun\n>")
+place = input("enter a place\n>")
+career = input("enter a career\n>")
+emotion = input("enter an emotion\n>")
+number = input("enter a number\n>")
+celeb = input("enter the name of a celeberity\n>")
+competition = input("enter a type of 1v1 competition\n>")
+goated = input("enter a name for good\n>")
+object = input("enter an object\n>")
+bodypart1 = input("enter a body part\n>")
+strike = input("enter a type of strike\n>")
+bodypart2 = input("enter another body part\n>")
+print(f"in the year {year}, there lived a man named {name} {noun}. {name} {noun} lives in {place} and works in {career}. {name} {noun} was sick of his {emotion} life so he set off on an adventure. after {number} years of searching foe something exciting {name} {noun} met {celeb}. {celeb} challenged {name} {noun} to a {competition}. turns out {name} {noun} was {goated} at {competition} so much so that after the {competition} {celeb} wanted to get active. {celeb} pulled out a {object} and swung it at {name} {noun}'s {bodypart1}. {name} {noun} evades the {object} then delivers a devestating {strike} to {celeb}'s {bodypart2} ending them")
