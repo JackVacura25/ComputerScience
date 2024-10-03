3e96460add81ae04140a4152658df7417c8962f5
819581@mystma.org <819581@mystma.org>
2024-10-03 15:10:56 -0500

    Test

---
+++ Unit 2 Functions And Variables/functions.py
@@ -0,0 +1,17 @@
+def add(m1, m2, m3, m4, m5):
+
+    def top5movies():       #Define the function with a name
+        print("1. " + m1)
+        print("2. " + m2)
+        print("3. " + m3)
+        print("4. " + m4)
+        print("5. " + m5)
+
+def add(x,y):
+    print(x + y)
+
+add(5,10)
+add(2,2)
+add(15,15)
+add("cat","dog")
+
