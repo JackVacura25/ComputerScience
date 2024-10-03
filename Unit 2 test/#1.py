ae852a51638843e21d6a534c3a202bc0aa7f5997
819581@mystma.org <819581@mystma.org>
2024-10-03 15:16:47 -0500

    unit2test

---
+++ Unit 2 test/#1.py
@@ -0,0 +1,4 @@
+word1  = input("please enter one word\n>")
+word2 = input("please enter another word\n>")
+word3 = input("alright one more word\n>") 
+print(f"so your words were {word1}, {word2}, and {word3}.") 
---
+++ tests/unit2test.py
@@ -0,0 +1,23 @@
+word1  = input("please enter one word\n>")
+word2 = input("please enter another word\n>")
+word3 = input("alright one more word\n>") 
+print(f"so your words were {word1}, {word2}, and {word3}.") 
+
+#UNFINISHED DONT WASTE YOUR TIME OSOWSKI
+num1  = input("please enter one number\n>")
+num2 = input("please enter another number\n>")
+num3 = input("alright one more number\n>") 
+
+def add(x, y, z):
+    num1  = input("please enter one number\n>")
+    num2 = input("please enter another number\n>")
+    num3 = input("alright one more number\n>") 
+
+    def add_three():
+       print(f"{num1}")
+
+def add(x, y, z):
+    print(x + y + z)
+    print
+
+# UNFINISHED! DONT WASTE YOUR TIME OSOWSKI
